b03569bff80491f5634004c2db5fdd1d424a0e5a
Ahmedscreativeverse <olugbasaahmed@gmail.com>
2026-03-17 13:14:02 +0100

    Gate Two

---
+++ python/Gate 2/task_one.py
@@ -0,0 +1,9 @@
+
+
+
+
+for number in range(1, 11):
+    
+    
+    
+    print(number)
---
+++ python/Gate 2/task_seven.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+sum = 0
+
+
+for value in range(1, 6):
+    
+    
+
+    number = int(input("Enter five numbers: "))
+    
+    
+
+    sum += number
+
+
+print("The sum of the numbers entered is:", sum)
