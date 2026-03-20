97872670d9615dcd06b5c38e9f7b44bd81ca6d6c
Ahmed Olugbasa <166473506+Ahmedscreativeverse@users.noreply.github.com>
2026-03-20 11:54:50 +0100

    Add test for replaceNegativesWithZero method

--- Java/Gate Four/TestFileLevel5.java
+++ Java/Gate Four/TestFileLevel5.java
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -28,7 +23,15 @@ public class TestFileLevel5 {
     }
 }
     
-    
+        //2
+ @Test    
+    public void testReplaceNegatives() {
+        int[] actual = MethodsLevelFive2.replaceNegativesWithZero{5, -9, 3, -6, 2, -11};
+        int[] expected = {5, 0, 3, 0, 2, 0};
+        
+        assertArrayEquals(expected, actual);
+    }
+}
     
     
     
