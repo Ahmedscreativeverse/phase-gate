f4ed1d5f3024d151233f1920347e99583d0b9b75
Ahmedscreativeverse <olugbasaahmed@gmail.com>
2026-03-20 10:36:34 +0100

    first commit

---
+++ Java/Gate Four/MethodsLevelFive.java
@@ -0,0 +1,74 @@
+
+
+
+public class MethodsLevelFive {
+
+    public static int[] getPrimes(int[] numbers) {
+      
+        int[] primeNumbers = new int[numbers.length]; 
+      
+        int count = 0; 
+
+        
+        for (int index = 0; index < numbers.length; index++) {
+      
+            int value = numbers[index]; 
+      
+      
+            boolean isPrime = true;
+
+            
+            if (value <= 1) {
+        
+                isPrime = false;
+            }
+
+           
+            for (int divisor = 2; divisor < value; divisor++) {
+        
+                if (value % divisor == 0) {
+        
+                    isPrime = false;
+                }
+            }
+
+            
+            if (isPrime) {
+        
+                primeNumbers[count] = value;
+        
+                count++;
+            }
+        }
+
+       
+        for (int currentIndex = 0; currentIndex < count; currentIndex++) {
+ 
+            for (int nextIndex = currentIndex + 1; nextIndex < count; nextIndex++) {
+ 
+                if (primeNumbers[currentIndex] > primeNumbers[nextIndex]) {
+ 
+                    int temp = primeNumbers[currentIndex];
+ 
+                    primeNumbers[currentIndex] = primeNumbers[nextIndex];
+ 
+                    primeNumbers[nextIndex] = temp;
+                }
+            }
+        }
+
+        
+        int[] exactPrimes = new int[count];
+ 
+        for (int counter = 0; counter < count; counter++) {
+ 
+            exactPrimes[counter] = primeNumbers[counter];
+        }
+
+      
+
+        return exactPrimes;
+    }
+}
+
+}
---
+++ Java/Gate Four/TestFileLevel5.java
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+
+public class TestFileLevel5 {
+
+    @Test
+       
+   
+    public void testGetPrimes() {
+       
+       
+        int[] actual = MethodsLevelFive.getPrimes(new int[]{5, 9, 3, 6, 2});
+       
+        int[] expected = {2, 3, 5};
+
+        assertArrayEquals(expected, actual);
+    }
+}
+    
+    
+    
+    
+    
+    
+       
+
