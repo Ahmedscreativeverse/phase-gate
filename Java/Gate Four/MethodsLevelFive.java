77603c3a660af33bbc19711ede581a7a0ea77f18
Ahmed Olugbasa <166473506+Ahmedscreativeverse@users.noreply.github.com>
2026-03-20 11:51:52 +0100

    Update MethodsLevelFive.java

--- Java/Gate Four/MethodsLevelFive.java
+++ Java/Gate Four/MethodsLevelFive.java
@@ -72,3 +72,28 @@ public class MethodsLevelFive {
 }
 
 }
+
+                    //2
+
+
+public class MethodsLevelFive2 {
+
+    public static int [] replaceNegativesWithZero(int [] scores) {
+    
+    for (int index = 0; index <scores.length; index++){
+    
+    int value = scores[index];
+    
+    if (value < 0) {
+    
+        scores[index] = 0;
+        
+        }
+        }
+        
+        return scores;
+        }
+    
+    }
+
+      
