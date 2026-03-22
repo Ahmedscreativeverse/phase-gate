7b31297553bf97908c1f9f7a28c2340c4f5ebec7
Ahmed Olugbasa <166473506+Ahmedscreativeverse@users.noreply.github.com>
2026-03-22 22:57:29 +0100

    Update WeekendSnacksMethod.java

--- Java/Gate Four/Milky Doughnut/WeekendSnacksMethod.java
+++ Java/Gate Four/Milky Doughnut/WeekendSnacksMethod.java
@@ -2,24 +2,24 @@
 
 public class WeekendSnacksMethod {
 
-//    public static int countEvens (int [] numbers){
-//    
-//   int numberOfEvenElement=0;
-//    
-//    for (int index =0; index<numbers.length; index++){
-//    
-//    if (numbers[index]% 2==0){
-//    
-//        numberOfEvenElement++;
-//    
-//    }
-//    }
-//    
-//    return numberOfEvenElement;
-//    }
-//    
-//    }
-//    
+   public static int countEvens (int [] numbers){
+   
+  int numberOfEvenElement=0;
+   
+   for (int index =0; index<numbers.length; index++){
+   
+   if (numbers[index]% 2==0){
+   
+       numberOfEvenElement++;
+   
+   }
+   }
+   
+   return numberOfEvenElement;
+   }
+   
+   }
+   
                     //2
                     
     public static int linearSearch (int []numbers, int target){
@@ -47,27 +47,27 @@ public class WeekendSnacksMethod {
                     
                     //3
                     
-//   
-//   public static int[] squares(int value) {
-//
-//    
-//    int[] perfectSquaresArray = new int[value];
-//
-//    
-//    for (int number = 1; number <= value; number++) {
-//
-//        perfectSquaresArray[number - 1] = number * number;
-//    }
-//
-//    return perfectSquaresArray;
-//}
-//   
-// 
-//   
-//   
-//   
-//   }
-//   
+  
+  public static int[] squares(int value) {
+
+   
+   int[] perfectSquaresArray = new int[value];
+
+   
+   for (int number = 1; number <= value; number++) {
+
+       perfectSquaresArray[number - 1] = number * number;
+   }
+
+   return perfectSquaresArray;
+}
+  
+
+  
+  
+  
+  }
+  
    
    
    
@@ -75,137 +75,137 @@ public class WeekendSnacksMethod {
             //4
             
             
-//  public static boolean arraysEquals (int [] value1, int [] value2){
-//  
-//                
-//   if (value1.length != value2.length){
-//  
-// 
-// return false;
-// }else{
-// 
-// return true;
-// }
-// 
-// for (int index = 0; index<value1.length; index++){
-// 
-// 
-// if (value1[index]!=value2[index]){
-// 
-// return false;
-// 
-// 
-// }
-// }
-//  
-//  return false;
-//  }
-//  
-//  
-//  }
-// 
-//  
-//  
-//  
-//  
-//                    //5
-//                    
-// public static int removeDuplicate (int [] arr ){
-//         
-//   for()
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         
-//         }           
-//                    
-//              
-//                    //6
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    
-//                    //7
-//                    
-//                    
-//                    
-//    public static int intersectOf (int [] value1, int value2)(){
-//    
-//    
-//    int theSameElement=0;
-//    
-//    for (int index = 0; index<value1.length; index++){
-//    
-//    
-//    
-//    if (index[value1]==value2){
-//    theSameElement++;
-//    
-//    
-//    
-//    }
-//    
-//    }
-//    
-//    
-//    
-//    }
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//    
-//
+ public static boolean arraysEquals (int [] value1, int [] value2){
+ 
+               
+  if (value1.length != value2.length){
+ 
+
+return false;
+}else{
+
+return true;
+}
+
+for (int index = 0; index<value1.length; index++){
+
+
+if (value1[index]!=value2[index]){
+
+return false;
+
+
+}
+}
+ 
+ return false;
+ }
+ 
+ 
+ }
+
+ 
+ 
+ 
+ 
+                   //5
+                   
+public static int removeDuplicate (int [] arr ){
+        
+  for()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }           
+                   
+             
+                   //6
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+                   //7
+                   
+                   
+                   
+   public static int intersectOf (int [] value1, int value2)(){
+   
+   
+   int theSameElement=0;
+   
+   for (int index = 0; index<value1.length; index++){
+   
+   
+   
+   if (index[value1]==value2){
+   theSameElement++;
+   
+   
+   
+   }
+   
+   }
+   
+   
+   
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
