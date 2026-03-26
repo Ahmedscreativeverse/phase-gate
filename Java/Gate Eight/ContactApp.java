6425229accc0ad084ef176a732291da135f7f410
Ahmedscreativeverse <olugbasaahmed@gmail.com>
2026-03-26 12:37:37 +0100

    firstcommit

---
+++ Java/Gate Eight/ContactApp.java
@@ -0,0 +1,22 @@
+
+
+public class ContactApp {
+    
+
+    public static void main(String[] args) {
+
+      
+    static void addContact() {
+        System.out.print("Enter Name: ");
+        String name = input.nextLine();
+
+        System.out.print("Enter Phone: ");
+        String phone = input.nextLine();
+
+        names.add(name);
+        phones.add(phone);
+
+        System.out.println("Added!");
+    }
+  
+    
---
+++ Java/Gate Eight/TestContactApp.java
@@ -0,0 +1,25 @@
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArraysEquals;
+
+        public class TestContactApp  {
+
+
+    @Test
+    
+    public void 
+
+
+
+
+
+
+
+
+
+
+}
